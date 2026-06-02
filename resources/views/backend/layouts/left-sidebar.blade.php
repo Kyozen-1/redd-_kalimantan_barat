f5bf4f5ccbbef890dea77f1bf3cd2ad5049d6a6d
Kyozen-1 <skadi1268@gmail.com>
2026-06-02 11:32:57 +0700

    done set left-sidebar

--- resources/views/backend/layouts/left-sidebar.blade.php
+++ resources/views/backend/layouts/left-sidebar.blade.php
@@ -5,30 +5,33 @@
         <div id="sidebar-menu">
 
             <ul class="metismenu" id="side-menu">
+                @if (auth()->user()->role == 'superadmin' || auth()->user()->role == 'admin')
+                    <li>
+                        <a href="{{ route('cms.dashboard.index') }}">
+                            <i class="mdi mdi-view-dashboard"></i>
+                            <span> Dashboard </span>
+                        </a>
+                    </li>
+                @endif
 
-                <li>
-                    <a href="{{ route('cms.dashboard.index') }}">
-                        <i class="mdi mdi-view-dashboard"></i>
-                        <span> Dashboard </span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="javascript: void(0);">
-                        <i class="mdi mdi-page-layout-sidebar-left"></i>
-                        <span> Layouts </span>
-                        <span class="menu-arrow"></span>
-                    </a>
-                    <ul class="nav-second-level" aria-expanded="false">
-                        <li><a href="layouts-horizontal.html">Horizontal</a></li>
-                        <li><a href="layouts-sidebar-sm.html">Small Sidebar</a></li>
-                        <li><a href="layouts-dark-sidebar.html">Dark Sidebar</a></li>
-                        <li><a href="layouts-dark-topbar.html">Dark Topbar</a></li>
-                        <li><a href="layouts-preloader.html">Preloader</a></li>
-                        <li><a href="layouts-sidebar-collapsed.html">Sidebar Collapsed</a></li>
-                        <li><a href="layouts-boxed.html">Boxed</a></li>
-                    </ul>
-                </li>
+                @if (auth()->user()->role == 'superadmin')
+                    <li>
+                        <a href="javascript: void(0);">
+                            <i class="mdi mdi-page-layout-sidebar-left"></i>
+                            <span> Layouts </span>
+                            <span class="menu-arrow"></span>
+                        </a>
+                        <ul class="nav-second-level" aria-expanded="false">
+                            <li><a href="layouts-horizontal.html">Horizontal</a></li>
+                            <li><a href="layouts-sidebar-sm.html">Small Sidebar</a></li>
+                            <li><a href="layouts-dark-sidebar.html">Dark Sidebar</a></li>
+                            <li><a href="layouts-dark-topbar.html">Dark Topbar</a></li>
+                            <li><a href="layouts-preloader.html">Preloader</a></li>
+                            <li><a href="layouts-sidebar-collapsed.html">Sidebar Collapsed</a></li>
+                            <li><a href="layouts-boxed.html">Boxed</a></li>
+                        </ul>
+                    </li>
+                @endif
             </ul>
 
         </div>

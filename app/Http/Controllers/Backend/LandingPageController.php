2059289752c3ce2505cf4ed0d64d5e21ed82a2ce
Kyozen-1 <skadi1268@gmail.com>
2026-06-17 14:10:09 +0700

    done

--- app/Http/Controllers/Backend/LandingPageController.php
+++ app/Http/Controllers/Backend/LandingPageController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
-use Illuminate\Support\Facades\File;
 use RealRashid\SweetAlert\Facades\Alert;
 use Intervention\Image\Laravel\Facades\Image;
 use Illuminate\Support\Facades\Storage;
